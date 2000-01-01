5a33d79febae0331055fa3528a95db46f4d09a35
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for eb6d6c72-59c7-4411-b91b-4486561d5227-bug-2

--- internal/store/digest.go
+++ internal/store/digest.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"sort"
+	"strings"
 
 	"archive-review/internal/domain"
 )
@@ -63,6 +64,62 @@ func (s *DiskStore) loadDigestIndex() error {
 			seenCases[id] = struct{}{}
 		}
 		s.digests[entry.Digest] = ids
+	}
+	return s.validateDigestIndexConsistency()
+}
+
+// validateDigestIndexConsistency cross-checks the in-memory digest index against the
+// case snapshots on disk so that corrupt persistent state is rejected at startup rather
+// than deferred until a case is read. It ensures every indexed case identifier resolves
+// to a readable snapshot whose ContentDigest matches the index entry, and that every
+// case snapshot is present in the index with a matching digest.
+func (s *DiskStore) validateDigestIndexConsistency() error {
+	visited := make(map[string]struct{}, len(s.digests))
+	entries, err := os.ReadDir(s.casesDir)
+	if err != nil {
+		return fmt.Errorf("枚举案件快照: %w", err)
+	}
+	for _, entry := range entries {
+		if entry.IsDir() || !strings.HasSuffix(entry.Name(), ".json") {
+			continue
+		}
+		id := strings.TrimSuffix(entry.Name(), ".json")
+		if !safeID.MatchString(id) {
+			return domain.DataInvalid("case_id", "案件目录包含无效快照文件名")
+		}
+		c, err := s.readCaseUnlocked(s.casePath(id))
+		if err != nil {
+			return domain.DataInvalid("case_snapshot", fmt.Sprintf("读取案件快照 %q 失败: %v", id, err))
+		}
+		indexed, ok := s.digests[c.ContentDigest]
+		if !ok {
+			return domain.DataInvalid("content_digest_index", fmt.Sprintf("案件快照 %q 的内容摘要未在索引中登记", id))
+		}
+		found := false
+		for _, indexedID := range indexed {
+			if indexedID == id {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return domain.DataInvalid("content_digest_index", fmt.Sprintf("案件快照 %q 未在内容摘要索引中登记", id))
+		}
+		visited[id] = struct{}{}
+	}
+	for digest, ids := range s.digests {
+		for _, id := range ids {
+			if _, ok := visited[id]; !ok {
+				return domain.DataInvalid("content_digest_index", fmt.Sprintf("内容摘要索引条目（摘要 %q）指向不存在的案件快照 %q", digest, id))
+			}
+			c, err := s.readCaseUnlocked(s.casePath(id))
+			if err != nil {
+				return domain.DataInvalid("content_digest_index", fmt.Sprintf("内容摘要索引指向无法读取的案件快照 %q: %v", id, err))
+			}
+			if c.ContentDigest != digest {
+				return domain.DataInvalid("content_digest_index", fmt.Sprintf("案件快照 %q 的内容摘要与索引条目不一致", id))
+			}
+		}
 	}
 	return nil
 }
